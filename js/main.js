4693a6c9cf00ba89a12504c5993ec567fff364c1
Yash Uppal <yashayash123@gmail.com>
2018-08-01 01:25:03 +0530

    add javascript

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//Listen for form submit
+
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e) {
+
+    //Get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+    
+
+    var bookmark = {
+        name: siteName,
+        url: siteUrl
+    }
+
+    
+
+    // Prevent form from submitting.
+    e.preventDefault();
+}
+
